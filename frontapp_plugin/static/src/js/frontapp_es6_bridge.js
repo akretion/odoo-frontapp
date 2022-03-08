96887ff4d2c9b1200662e364a61e6a73a2e66692
Raphaël Valyi <rvalyi@akretion.com>
2022-03-08 16:35:13 -0300

    auto JS OCA formatting

--- frontapp_plugin/static/src/js/frontapp_es6_bridge.js
+++ frontapp_plugin/static/src/js/frontapp_es6_bridge.js
@@ -4,5 +4,5 @@
 // So this trivial ES6 module is here to required the FrontApp module
 // and make Front available in window.Front for later use from non ES6 app.js...
 // Where is my full stack developper award?
-import Front from '@frontapp/plugin-sdk';
+import Front from "@frontapp/plugin-sdk";
 window.Front = Front;
--- frontapp_plugin/static/webpack.config.js
+++ frontapp_plugin/static/webpack.config.js
@@ -1,19 +1,21 @@
 module.exports = {
-    entry: './src/js/frontapp_es6_bridge.js',
+    entry: "./src/js/frontapp_es6_bridge.js",
     output: {
-        path: __dirname + '/dist',
-        filename: 'frontapp-plugin-sdk.js'
+        path: __dirname + "/dist",
+        filename: "frontapp-plugin-sdk.js",
     },
     module: {
-        rules: [{
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }
-        }]
-    }
-}
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+        ],
+    },
+};
