73bee9b12a091eb36835dc71bb1521e19bd98213
Raphaël Valyi <rvalyi@akretion.com>
2022-03-14 22:34:09 -0300

    better UI/more fields

--- frontapp_plugin/static/src/js/app.js
+++ frontapp_plugin/static/src/js/app.js
@@ -14,8 +14,7 @@ odoo.define("web.frontapp", function (require) {
         // -------------------------------------------------------------------------
         const actions = {
             addContact({state}, contact) {
-                console.log(state);
-                var title = contact.display_name;
+                console.log("ADD CONTACT", contact);
                 if (contact) {
                     state.contacts.push(contact);
                 }
@@ -68,10 +67,10 @@ odoo.define("web.frontapp", function (require) {
         // -------------------------------------------------------------------------
         // Opportunity Component
         // -------------------------------------------------------------------------
-        const OPPORTUNITY_TEMPLATE = xml/* xml */ `
+        const OPPORTUNITY_TEMPLATE = xml`
     <div class="opportunity">
-    OP:
-      <a t-att-href="props.opportunity.href" target="_blank">
+      <i class="fa fa-rocket" />
+      <a class="pl-1" t-att-href="props.opportunity.href" target="_blank">
         <t t-esc="props.opportunity.name"/>
       </a>
     </div>`;
@@ -85,15 +84,21 @@ odoo.define("web.frontapp", function (require) {
         // -------------------------------------------------------------------------
         // Contact Component
         // -------------------------------------------------------------------------
-        const CONTACT_TEMPLATE = xml/* xml */ `
+        const CONTACT_TEMPLATE = xml`
     <div class="contact" t-att-class="props.contact.isLinked ? 'linked' : ''">
         <div class="contact-info">
         <input type="checkbox" t-att-checked="props.contact.isLinked"
             t-att-id="props.contact.id"
             t-on-click="dispatch('toggleContactLink', props.contact.id)"/>
-
-        <a t-att-href="props.contact.href" target="_blank">
-          <t t-esc="props.contact.display_name"/>
+          
+        <t t-if="props.contact.company_type=='company'" >
+          <i class="fa fa-institution" />
+        </t>
+        <t t-else="" >
+          <i class="fa fa-address-card-o" />
+        </t>
+        <a class="pl-1" t-att-href="props.contact.href" target="_blank">
+          <t t-esc="props.contact.name"/>
         </a>
 
         <span class="delete" t-on-click="dispatch('deleteContact', props.contact.id)">🗑</span>
@@ -101,20 +106,45 @@ odoo.define("web.frontapp", function (require) {
         <div t-if="props.contact.function" >
           <t t-esc="props.contact.function" />
         </div>
-        <div t-if="props.contact.company_id" >
-          <t t-esc="props.contact.company_id.name" />
-        </div>
         <div t-if="props.contact.phone" >
-          <t t-esc="props.contact.phone" />
+            <i class="fa fa-phone" />
+            <span class="pl-1" >
+              <t t-esc="props.contact.phone" />
+            </span>
         </div>
         <div t-if="props.contact.mobile" >
-          <t t-esc="props.contact.mobile" />
+            <i class="fa fa-mobile-phone" />
+            <span class="pl-1" >
+              <t t-esc="props.contact.mobile" />
+            </span>
         </div>
-        <div t-if="props.contact.zip" >
-          <t t-esc="props.contact.zip" />
+        <div t-if="props.contact.parent_href" >
+          <i class="fa fa-institution" />
+          <span class="pl-1" >
+            <a class="pl-1" t-att-href="props.contact.parent_href" target="_blank">
+              <t t-esc="props.contact.parent_id[1]" />
+            </a>
+          </span>
         </div>
-        <div t-if="props.contact.city" >
-          <t t-esc="props.contact.city" />
+        <div>
+          <i class="fa fa-map-marker" />
+ 
+            <span class="pl-1" t-if="props.contact.zip" >
+              <t t-esc="props.contact.zip" />
+            </span>
+            <div t-if="props.contact.city" >
+              <t t-esc="props.contact.city" />
+            </div>
+            <div t-if="props.contact.country_id" >
+              <t t-esc="props.contact.country_id[1]" />
+            </div>
+
+        </div>
+        <div t-if="props.contact.user_id" >
+          <i class="fa fa-user" />
+          <span class="pl-1">
+            <t t-esc="props.contact.user_id[1]" />
+          </span>
         </div>
         <div class="opportunity-list">
             <Opportunity t-foreach="props.contact.opportunities" t-as="opportunity" t-key="opportunity.id" opportunity="opportunity"/>
@@ -131,7 +161,7 @@ odoo.define("web.frontapp", function (require) {
         // -------------------------------------------------------------------------
         // App Component
         // -------------------------------------------------------------------------
-        const APP_TEMPLATE = xml/* xml */ `
+        const APP_TEMPLATE = xml`
     <div class="frontapp-odoo">
         <div class="contact-list">
             <Contact t-foreach="displayedContacts" t-as="contact" t-key="contact.id" contact="contact"/>
