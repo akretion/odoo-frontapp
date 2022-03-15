6fb3a5412e83d479d9b92df90599c7105ebcac65
Raphaël Valyi <rvalyi@akretion.com>
2022-03-14 22:33:39 -0300

    fields and preparing for last conversation links

--- frontapp_plugin/models/res_partner.py
+++ frontapp_plugin/models/res_partner.py
@@ -2,16 +2,15 @@ from odoo import api, models
 
 PARTNER_LIMIT = 10
 LEAD_LIMIT = 5
-
+LAST_CONVERSATIONS_COUNT = 3
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     @api.model
     def _get_frontapp_partner_fields(self):
-        # TODO DS 'parent_privileged_distrib_id',
         return [
-            "display_name",
+            "name",
             "category_id",
             "phone",
             "mobile",
@@ -26,6 +25,7 @@ class ResPartner(models.Model):
             "category_id",
             "country_id",
             "user_id",
+            "company_type",
         ]
 
     @api.model
@@ -36,7 +36,6 @@ class ResPartner(models.Model):
     def _search_frontapp_partners(
         self, contact_emails, search_param, linked_partner_ids, domain=None
     ):
-        # TODO DS email_2
         if not domain:
             if search_param:
                 domain = [
@@ -71,7 +70,7 @@ class ResPartner(models.Model):
         if not frontapp_context:
             frontapp_context = {"conversation": {}}  # useful for local testing
         conversation_key = frontapp_context["conversation"].get("id", "no_conversation")
-        links = self._frontapp_conversations([], conversation_key)
+        links = self._frontapp_conversations([], conversation_key)[0]
         linked_partner_ids = [link.res_id for link in links]
 
         partner_records = self._search_frontapp_partners(
@@ -120,25 +119,40 @@ class ResPartner(models.Model):
                 "%s/web#id=%s&action=%s&model=res.partner&view_type=form&cids=&menu_id=%s"
                 % (odoo_server, partner["id"], odoo_partner_action, odoo_partner_menu)
             )
+            if partner.get("parent_id"):
+                partner["parent_href"] = (
+                "%s/web#id=%s&action=%s&model=crm.lead&view_type=form&cids=&menu_id=%s"
+                % (odoo_server, partner["parent_id"][0], odoo_partner_action, odoo_partner_menu)
+               )
             partner["conversation_id"] = conversation_key
-            if self._frontapp_conversations([partner["id"]], conversation_key):
+            if self._frontapp_conversations([partner["id"]], conversation_key)[0]:
                 partner["isLinked"] = True
         return partner_records
 
     def _frontapp_conversations(self, partner_ids, conversation_key=False):
         domain = [
             ("model", "=", "res.partner"),
-            (
-                "subtype_id",
-                "=",
-                self.env.ref("frontapp_plugin.frontapp_conversation_link").id,
-            ),
+            ("subtype_id", "=", self.env.ref("frontapp_plugin.frontapp_conversation_link").id),
         ]
         if partner_ids:
             domain.append(("res_id", "in", partner_ids))
+        domain_other = domain
         if conversation_key:
             domain.append(("frontapp_conversation_key", "=", conversation_key))
-        return self.env["mail.message"].search(domain)
+        related_conversations = self.env["mail.message"].search(
+            domain,
+            order="write_date DESC",
+        )
+
+        domain_other.append(('id', 'not in', related_conversations.mapped("id")))
+        if related_conversations and not partner_ids:
+            domain_other.append(("res_id", "=", related_conversations[0].res_id))
+        other_conversations = self.env["mail.message"].search(
+            domain_other,
+            order="write_date DESC",
+            limit=LAST_CONVERSATIONS_COUNT,
+        )
+        return (related_conversations, other_conversations)
 
     def toggle_contact_link(self, is_linked, frontapp_context):
         if not frontapp_context:
@@ -149,7 +163,7 @@ class ResPartner(models.Model):
         for partner in self:
             existing_links = self._frontapp_conversations(
                 [partner.id], conversation_key
-            )
+            )[0]
             if is_linked:
                 if not existing_links:
                     body = """<h3>%s</h3><a class="frontapp_conversation_link" target="_blank" href="https://app.frontapp.com/open/%s">%s...</a>""" % (
